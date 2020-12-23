2c30321c11d537ab20ac95b7ada53d04fac9fdd0
damou_ <mouadsehbaoui@gmail.com>
2020-12-23 15:01:43 +0100

    forgot to add this

---
+++ frontend/app/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about col-4 offset-4 mt-5">
+    <h1 class="text-danger">This is an about page</h1>
+  </div>
+</template>
---
+++ frontend/app/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    {{ this.$store.commit("email", 'sortirLesKetes@gmail.com') }}
+    <p class="text-danger col-7 offset-6">{{ this.$store.state.email }}</p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
